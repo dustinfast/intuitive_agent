60852739aa092dd98ef68c10f5e4e0fdc689aed3
dustinfast <dustin.fast@hotmail.com>
2018-09-18 20:29:55 -0500

    Added agent and genetic files -DF

---
+++ agent.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+""" 
+    Conventions:
+        x - Input layer (i.e. the set of input-layer nodes)
+        y - Output layer
+        t - Some arbitrary tensor
+
+    The agent layers:
+        
+
+    # TODO: 
+        
+
+    Author: Dustin Fast, 2018
+"""
