21228dec8639d8ab3703de798cb69671c1baa1d0
dustinfast <dustin.fast@hotmail.com>
2018-09-19 18:43:50 -0500

    Added rough psuedo lines to agent.py -DF

--- agent.py
+++ agent.py
@@ -13,3 +13,22 @@
 
     Author: Dustin Fast, 2018
 """
+
+# Suppose data sets A, B, C
+
+# Train sub-ann[0] on A, sub-ann[1] on B, sub_ann[2] on C, etc...
+
+# Train genetic sub-layer on training set A U B U C with classifier kernel
+
+# Train attentive layer on same training set A U B U C
+
+# Override genetic sub-layer fitness function
+
+# Run agent
+
+# On intutive layer discover new connection in validation set A U B U C ->
+# signal good fitness to genetic sub-layer. This represents feedback from env.
+
+# On genetic sublayer recv good fitness? 
+
+
