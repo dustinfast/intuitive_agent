244a173dcf32e8aff762f5dbe937f29950687992
dustinfast <dustin.fast@hotmail.com>
2018-09-20 21:22:15 -0500

    Class persistence working via set_labels -DF

--- ann.py
+++ ann.py
@@ -24,13 +24,10 @@
         t = A tensor
 
     # TODO: 
-        Iniital bias = -1, initial bias wt = .5 
-        Encapsulate main contents into ANN.from_csv()
         Noise Params
         Fails if some class types missing between training and validation set.
         Expand ANN to allow an arbitrary number of hidden layers
         Implement ability to use a PyTorch.utils.data.DataLoader as data source
-        Ensure self.classes is persistent
 
 
     Author: Dustin Fast, 2018
@@ -42,6 +39,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torch.autograd import Variable as V
+import logging
 import pandas as pd
 
 from classlib import Model
@@ -132,7 +130,7 @@ class ANN(nn.Module):
         super(ANN, self).__init__()
         self.ID = ID
         self.persist = persist
-        self.classes = None  # Unique class labels, set on self.train()
+        self.class_labels = None
         
         # Layer defs
         self.x_sz = dims[0]
@@ -174,8 +172,16 @@ class ANN(nn.Module):
     def _label_from_outputs(self, outputs):
         """ Given an outputs tensor, returns the mapped classification label.
         """
-        _, idx = torch.max(outputs, 0)
-        return self.classes[idx]
+        try:
+            _, idx = torch.max(outputs, 0)
+            return self.class_labels[idx]
+        except TypeError:
+            self.model.log('Must set class labels first.', logging.error)
+
+    def set_labels(self, data):
+        """ Sets the class labels from the given DataFromCSV object.
+        """
+        self.class_labels = data.classes
 
     def forward(self, t):
         """ Feeds the given tensor through the ANN, thus updating output layer.
@@ -214,11 +220,11 @@ class ANN(nn.Module):
                 self.model.log('Epoch {} - loss: {}'.format(epoch, curr_loss))
 
         # If no class labels yet, use the dataset's.
-        if not self.classes:
-            self.classes = data.classes
-            self.model.log('Set self.classes:' + str(self.classes))
+        if not self.class_labels:
+            self.set_labels(data)
+            self.model.log('Set class labels: ' + str(self.class_labels))
         else:
-            self.model.log('Still using old self.classes:' + str(self.classes))
+            self.model.log('Using existing labels: ' + str(self.class_labels))
 
         self.model.log('Training Completed: ' + info_str + '\n')
 
@@ -229,20 +235,25 @@ class ANN(nn.Module):
     def validate(self, data, verbose=False):
         """ Validates the ANN against the given data set.
         """
+        if not self.class_labels:
+            self.model.log('No class labels defined.', logging.error)
+            return
+
         info_str = 'file={}.'.format(data.fname)
         self.model.log('Validation started: ' + info_str)
 
         total = 0
         corr = 0
-        class_total = {c: 0 for c in self.classes}
-        class_corr = {c: 0 for c in self.classes}
+        class_total = {c: 0 for c in self.class_labels}
+        class_corr = {c: 0 for c in self.class_labels}
 
         with torch.no_grad():
             for row in data:
                 inputs, target = iter(row)
-                outputs = self(inputs)
+                # outputs = self(inputs)
+                pred_class = self.classify(inputs)
                 target_class = self._label_from_outputs(target)
-                pred_class = self._label_from_outputs(outputs)
+                # pred_class = self._label_from_outputs(outputs)
                 
                 total += 1
                 class_total[target_class] += 1
@@ -256,7 +267,7 @@ class ANN(nn.Module):
         if verbose:
             log_str += 'Correct: %d\n' % corr
             log_str += 'Total: %d\n' % total
-            for c in self.classes:
+            for c in self.class_labels:
                 log_str += '%s : %d / %d ' % (c, class_corr[c], class_total[c])
                 if class_total[c] > 0:
                     log_str += '(%d%%)' % (100 * class_corr[c] / class_total[c])
@@ -273,28 +284,33 @@ class ANN(nn.Module):
 
 
 if __name__ == '__main__':
-    # Define and load training and validation sets
+    # Load the training and validation data sets
     trainfile = 'static/datasets/letter_train.data'
-    # trainfile = 'static/datasets/test.data'  # debug
+    trainfile = 'static/datasets/test3.data'  # debug
     valfile = 'static/datasets/letter_val.data'
-    # valfile = 'static/datasets/test.data'  # debug
+    valfile = 'static/datasets/test3.data'  # debug
     train_data = DataFromCSV(trainfile, (0, 15))
     val_data = DataFromCSV(valfile, (0, 15))
 
-    # The ANN's layer sizes. In/out sz is based on the dataset's dimennsions
+    # Define the ANN's layer sizes.
     x_sz = train_data.feature_count
     h_sz = 14
     y_sz = train_data.class_count
     ann_dimens = (x_sz, h_sz, y_sz)
 
-    # Init, train, and subsequently validate the ANN
-    ann = ANN('ann.1.9', ann_dimens, console_out=True, persist=True)
-    # ann.train(train_data, epochs=10000, lr=.1, alpha=.9, stats_at=10, noise=None)
-    ann.train(train_data, epochs=1000, lr=.1, alpha=.9, stats_at=100, noise=None)
+    # Init the ann
+    ann = ANN('test_ann', ann_dimens, console_out=True, persist=True)
+
+    # Train the ann with the training set
+    ann.train(train_data, epochs=100, lr=.1, alpha=.9, stats_at=100, noise=None)
+    
+    # Set the classifier labels (only really necessary if loading pre-trained)
+    ann.set_labels(train_data)
+
+    # Validate the ann against the validation set
     ann.validate(val_data, verbose=True)
 
-    # Example of a classification request, given a feature vector for "b"
-    b_inputs = torch.tensor(
-        [4, 2, 5, 4, 4, 8, 7, 6, 6, 7, 6, 6, 2, 8, 7, 10], dtype=torch.float)
-    prediction = ann.classify(b_inputs)
-    print('Test Classification: ' + prediction)  # Should print "B"
+    # Example of a classification request, given a feature vector for "B"
+    inputs = torch.tensor([4,2,5,4,4,8,7,6,6,7,6,6,2,8,7,10], dtype=torch.float)
+    prediction = ann.classify(inputs)
+    print('Test Classification: ' + str(prediction))  # Should print "B"
