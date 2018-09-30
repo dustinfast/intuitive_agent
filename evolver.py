7c007fa098664729c89f8025ceb4ed20efbb5eda
dustinfast <dustin.fast@hotmail.com>
2018-09-30 01:37:07 -0500

    Evolver training working -DF

--- evolver.py
+++ evolver.py
@@ -24,7 +24,7 @@
 import sys; sys.path.append('lib')
 import karoo_gp.karoo_gp_base_class as karoo_gp
 
-from classlib import ModelHandler
+# from classlib import ModelHandler
 
 MODEL_EXT = '.tree'
 
@@ -33,12 +33,12 @@ class KarooEvolve(karoo_gp.Base_GP):
         Based on https://github.com/kstaats/karoo_gp.py.
     """
     def __init__(self, 
-                 kernel='c',            # (c)lassifier, (r)egression, or (m)atching
+                 kernel='r',            # (c)lassifier, (r)egression, or (m)atching
                  display='m',           # (i)nteractive, (g)eneration, (m)iminal, (s)ilent, or (d)e(b)ug
-                 tree_pop_max=50,       # Maximum population size
+                 tree_pop_max=10,       # Maximum population size
                  tree_depth_min=3,      # Min nodes of any tree
-                 tree_depth_max=3,      # Max tree depth
-                 generation_max=1,      # Max generations to evolve
+                 tree_depth_max=10,     # Max tree depth
+                 generation_max=10,     # Max generations to evolve
                  tourn_size=10,         # Individuals in each "tournament"
                  precision=6,           # Float points for fx_fitness_eval
                  menu=False             # Denotes Karoo GP "pause" menu enabled
@@ -57,8 +57,8 @@ class KarooEvolve(karoo_gp.Base_GP):
 
         # Init ratio of mutation types to be applied
         self.evolve_repro = int(0.1 * self.tree_pop_max)    # Reproductive
-        self.evolve_point = int(0.0 * self.tree_pop_max)    # Point
-        self.evolve_branch = int(0.2 * self.tree_pop_max)   # Branch
+        self.evolve_point = int(0.1 * self.tree_pop_max)    # Point
+        self.evolve_branch = int(0.1 * self.tree_pop_max)   # Branch
         self.evolve_cross = int(0.7 * self.tree_pop_max)    # Crossover
 
     def get_best(self, tourn_size=10):
@@ -66,7 +66,7 @@ class KarooEvolve(karoo_gp.Base_GP):
         """
         return self.fx_fitness_tournament(tourn_size)
 
-    def gen_first_pop(self, datafile, tree_type='f', tree_depth_base=3):
+    def gen_first_pop(self, datafile, tree_type='r', tree_depth_base=5):
         """ Generates the initial population tree and fitness function.
             tree_type (char)        : (f)ull, (g)row, or (r)amped 50/50
             tree_depth_base (int)   : Initial population tree's depth
@@ -102,7 +102,7 @@ class KarooEvolve(karoo_gp.Base_GP):
         # Evolve population trees for each generation specified
         for self.generation_id in range(2, self.generation_max + 1):
             self.population_b = []          # The evolving/next generation
-            self.fx_fitness_gene_pool()     # Init gene pool contstraints
+            self.fx_fitness_gene_pool()     # Init gene pool consttraints
             self.fx_karoo_reproduce()       # Do reproduction
             self.fx_karoo_point_mutate()    # Do point mutation
             self.fx_karoo_branch_mutate()   # Do branch mutation
@@ -114,6 +114,22 @@ class KarooEvolve(karoo_gp.Base_GP):
         
         # Done generating populations...
         self._show_menu()
+    
+    def next_pop(self):
+        """ Returns a new population bred from the current population. 
+            Differs from _gen_next_pop in that no "learning" takes place.
+        """
+        # Evolve population trees for each generation specified
+        self.population_b = []          # The evolving/next generation
+        self.fx_fitness_gene_pool()     # Init gene pool consttraints
+        self.fx_karoo_reproduce()       # Do reproduction
+        self.fx_karoo_point_mutate()    # Do point mutation
+        self.fx_karoo_branch_mutate()   # Do branch mutation
+        self.fx_karoo_crossover()       # Do crossover reproduction
+        self.fx_eval_generation()       # Eval all trees of curr generation
+
+        # Set curr population to the newly evolved population
+        return self.population_b
 
     def _show_menu(self):
         """ Displays the Karoo "pause" menu, iff enabled.
@@ -122,49 +138,10 @@ class KarooEvolve(karoo_gp.Base_GP):
             self.fx_karoo_eol()
 
 
-# class TensorEvolver(object):
-#     """ A handler for element-wise tensor operations according to a genetically
-#         evolving expression tree.
-#     """
-#     def __init__(self, ID, console_out, persist):
-#         """ ID (str)                : This Evolver's unique ID number
-#             console_out (bool)      : Output log stmts to console flag
-#             persist (bool)          : Persit mode flag
-#         """
-#         # Generic object params
-#         self.ID = ID
-#         self.model_file = None
-#         self.persist = persist
-
-#         self.evolver = KarooEvolve(menu=False)     # The karoo_gp interface
-#         self.expression = None                     # The current expression tree
-
-#         # Init the load, save, log, and console output handler
-#         # f_save = "self.evolver.fx_archive_tree_write(self.evolver.population_a, 'a')"
-#         # f_load = self.evolver.fx_archive_params_write('Desktop')
-#         # self.ModelHandler = ModelHandler(self,
-#         #                    console_out,
-#         #                    persist)
-#                         #    model_ext=MODEL_EXT,
-#                         #    save_func=f_save,
-#                         #    load_func=f_load)
-
-#     def __str__(self):
-#         str_out = 'ID = ' + self.ID
-#         # str_out += 'Data = ' + str(self.array)
-#         return str_out
-
-#     def forward(self, tensors):
-#         """ Returns the given list of tensors after performing element-wise
-#             operations on them according to the current expression tree.
-#         """
-#         raise NotImplementedError
-
-
-class Evolver(object):
-    """ A handler for evolving a string of binary digits of the given length.
+class AttentiveEvolver(object):
+    """ A handler for an evolving expression tree in the context of the 
+        intutive agent.
     """
-
     def __init__(self, ID, console_out, persist):
         """ ID (str)                : This Evolver's unique ID number
             console_out (bool)      : Output log stmts to console flag
@@ -172,35 +149,56 @@ class Evolver(object):
         """
         # Generic object params
         self.ID = ID
+        self.model_file = None
         self.persist = persist
-        # self.length = length
-        self.string = None
 
-        self.evolver = KarooEvolve(menu=False)     # The karoo_gp interface
-        self.expression = None                     # The current expression tree
-
-        # Init the load, save, log, and console output handler
-        # f_save = "self.evolver.fx_archive_tree_write(self.evolver.population_a, 'a')"
-        # f_load = self.evolver.fx_archive_params_write('Desktop')
-        # self.ModelHandler = ModelHandler(self,
-        #                    console_out,
-        #                    persist)
-        #    model_ext=MODEL_EXT,
-        #    save_func=f_save,
-        #    load_func=f_load)
+        # The karoo_gp interface
+        self.evolver = KarooEvolve(display='m',
+                                   kernel='r',
+                                   tree_pop_max=50,
+                                   tree_depth_min=15,
+                                   tree_depth_max=10,
+                                   generation_max=2,
+                                #    generation_max=20,
+                                   menu=False)  
 
     def __str__(self):
-        str_out = 'ID = ' + self.ID
-        # str_out += 'Data = ' + str(self.array)
-        return str_out
+        return 'ID = ' + self.ID
 
-    def forward(self, tensors):
-        """ Returns the given list of tensors after performing element-wise
-            operations on them according to the current expression tree.
+    def train(self, datafile):
+        """ Trains the evolver from the given data file.
+            Accepts:
+                datafile (str)      : The data file name/path
         """
-        raise NotImplementedError
+        self.evolver.gen_first_pop(datafile=datafile,
+                                   tree_type='r', 
+                                   tree_depth_base=5)
+
+    def forward(self, inputs):
+        """ Performs the evolver's expressions on the given inputs.
+            Returns a list of lists, one for each of the evolvers expressions.
+        """
+        results = []    # Results container
+        processed = []  # Contains evaluated expressions, to avoid duplicates
+
+        # Advance the population
+        population = self.evolver.next_pop()
+
+        # Iterate each 
+        for tree in range(1, len(population)):
+            self.evolver.fx_eval_poly(population[tree])
+            
+            if self.evolver.algo_sym in processed:
+                continue
+
+            # alg_sym will be an expr str. Ex: -C - 3*D + 2*s - E
+            expr = self.evolver.algo_sym.replace('', '')
+            result = inputs
+
+
+
        
     
 if __name__ == '__main__':
-    ev = Evolver('test_evolver', console_out=True, persist=False)
-    ev.evolver.gen_first_pop(datafile='static/datasets/test_r.csv')
+    ev = AttentiveEvolver('test_evolver', console_out=True, persist=False)
+    ev.train('static/datasets/nouns_sum.csv')
