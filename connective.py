0c1d17106a586e6392d2d2ab742a5eeaedaa5d84
dustinfast <dustin.fast@hotmail.com>
2018-10-05 14:39:15 -0500

    Added is_valid docstring -DF

--- connective.py
+++ connective.py
@@ -23,31 +23,41 @@ import requests
 MODEL_EXT = '.cn'
 
 class Connective(object):
-    def __init__(self):
-        pass
+    def __init__(self, ID):
+        self.mode = mode
+        self.success = success
     
-    def is_connective(self, data):
-        """ Returns True iff the given data elements can be verified as having
-            some conceptual connection.
+    def is_valid(self, data, try_funcs, succ_funcs=[]):
+        """ Returns True iff any f(data['string']) returns true for any f in
+            try_funcs. Additionally, for each f evaluating to True in this way, 
+            calls f(data['success_args']) for every f in succ_funcs.
             Accepts:
-                data (str or list)  : A string or list of elements
-                metric (function)   : The function to use as a comparator
+                data (dict)       : Must contain at least two keys: 'string'
+                                    and 'success_args', where 'string' is the
+                                    string to be evaluated and 'success_args'
+                                    are the arguments to any f in success_funcs.
+                try_funcs (list)  : Functions for evaluating each 
+                                    data['string'] (ex: Logical.is_noun).
+                succ_funcs(list)  : Functions to call when any f in try_funcs
+                                    returns True. Must accept a single argument
+                                    of the type given by data['success_args'].
         """
-        if type(data) is str:
-            return self.is_noun(data)
-        else:
-            raise Exception("Unhandled data type encountered.")
-
-    #TODO: def is_valid_python():
-        # eval. If success, add to KB, by domain label = python
+        
 
     @staticmethod
-    def is_noun(word, lang='en'):
-        """ Returns True iff the given word is a noun in the english language.
+    def is_python(string):
+        """ Returns true iff the given string is a valid python string.
+            Accepts:
+                string
+        """
+
+    @staticmethod
+    def is_noun(string, lang='en'):
+        """ Returns True iff the given string is a noun in the english language.
             Results are obtained from OxfordDictionaries.com via HTTP GET.
             For API info, see: https://developer.oxforddictionaries.com
             Accepts:
-                word (str)      : The word of interest
+                string (str)    : The word of interest
                 lang (str)      : Language. Ex: en = english, es = spanish, etc.
         """
         # OxfordDictionaries.com API credentials
