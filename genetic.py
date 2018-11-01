c19f336e3836895d3b38c0b3e9b3e3adb73041e4
dustinfast <dustin.fast@hotmail.com>
2018-11-01 07:40:30 -0500

    Removed mode 1 and fixed mratio load from file bug -DF

--- genetic.py
+++ genetic.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-""" A module for genetically evolving a population of expression trees using 
-    and applying them to user-specified data in predefined ways.
-    
+""" A module for genetically evolving a population of expression trees 
+    and applying them to user-specified data.
+
     Module Structure:
         GPM is the main interface.
         Persistence and output handled by sharedlib.ModelHandler.
+    
+    Usage: 
+        See main() for example usage.
         
     Karoo GP Version Info: 
         Karoo GP was written by Kai Staats for Python 2.7. We use the adapted 
@@ -13,34 +16,30 @@
         Fast for use in this module (see notes in 'lib/karoo_gp_base_class.py')
 
     # TODO: 
-       
 
     Author: Dustin Fast, 2018
 """
 
 import re
 from numpy import array
-from random import random
 import sys; sys.path.append('lib')
 
 import karoo_gp.karoo_gp_base_class as karoo_gp
-from sharedlib import ModelHandler, AttributesIter, negate
+from sharedlib import ModelHandler, AttrIter, negate
 
 
 MODEL_EXT = '.ev'           # File extensions for model file save/load
 OP_FLIP_STR = '+ abs'       # Alphabetic case flip operator in string form
-KERNEL_MODES = [1, 2, 3]    # See class docstring for mode descriptions
-
-ATTRIB_OUTPUT = 'output'            # Attribute label
-ATTRIB_INCONTEXT = 'from_inputs'    # Attribute label
-
+KERNEL_MODES = [1, 2]       # See class docstring for kernel descriptions
+KERNEL_OPERATORS = {1: [['+', '2']],
+                    2: [['+', '2'],
+                        ['+ abs', '2']]}
 
 class Genetic(karoo_gp.Base_GP):
-    """ An evolving population of expression trees with methods for applying
-        them to supplied data and reproducing based on fitness according to
+    """ An evolving population of genetically evolving expression trees with 
+        methods for applying them as a "mask" to the supplied data according to
         the given kernel:
-        Kernel 1: Numeric minimizing kernel with +, -, *, and / operators
-        Kernel 2: String masking kernel with + operator
+        Kernel 1: String masking kernel with + operator
         Kernel 3: String masking kernel with + and "case flip" operators
     """
     def __init__(self, ID, kernel, max_pop, max_depth, max_inputs, tourn_sz=10,
@@ -54,23 +53,53 @@ class Genetic(karoo_gp.Base_GP):
             console_out (bool)      : Output log stmts to console flag
             persist (bool)          : Persist mode flag
         """
+        if kernel not in KERNEL_MODES:
+            raise AttributeError('Invalid kernel specified.')
+            
         super(Genetic, self).__init__()
         self.ID = ID
         self.persist = persist
         self.tree_pop_max = max_pop
         self.tree_depth_max = max_depth
         self.tourn_size = tourn_sz
-
-        self._set_mratio()                    # Set initial mutation ratios
-        self._set_kernel(kernel, max_inputs)  # Setup the specified kernel
+        self.display = 's'          # Silence Karoo GP menus/output
+        self.precision = 6          # Tourney fitness floating points
+        self.fitness_type = 'max'   # Maximizing kernel function
+        self.functions = array(KERNEL_OPERATORS.get(kernel))
+        self._set_mratio()  # Set initial mutation ratios
         
-        # Init the load, save, log, and console output handler if none given
+        # Init terminal symbols - We work with string inputs so terminals
+        # must be non-numeric (or sympy will simplify them) and not sympy
+        # func names (or sympy will try to call them). Ex: 'bzzz', 'gh', etc.
+        self.terminals = []
+        trailing_chars = 1
+        curr_char = 0
+        for _ in range(1, max_inputs + 1):
+            curr_char += 1
+            if curr_char >= 26:
+                trailing_chars += 1
+                curr_char = 1
+            ch = curr_char + 96
+            self.terminals.append(chr(ch) + chr(ch + 1) * trailing_chars)
+        self.terminals += ['s']  # 's' label unused but required by KarooGP
+
+        # Init the load, save, log, and console output handler
         f_save = "self.save('MODEL_FILE')"
         f_load = "self.load('MODEL_FILE')"
         self.model = ModelHandler(self, console_out, persist,
                                   model_ext=MODEL_EXT,
                                   save_func=f_save,
                                   load_func=f_load)
+
+        # Init first generation if not already loaded by ModelHandler
+        try:
+            self.population_a
+        except AttributeError:
+            self.generation_id = 1
+            self.population_a = ['Generation 1']
+            self.fx_karoo_construct('r', 6)  # r = most diverse initial pop
+            for treeID in range(1, len(self.population_a)):
+                self.population_a[treeID][12][1] = 0.0
     
     def __str__(self):
         str_out = '\nID = ' + self.ID + '\nSize = (\n  '
@@ -80,82 +109,24 @@ class Genetic(karoo_gp.Base_GP):
         str_out += 'evolve_repro : ' + str(self.evolve_repro) + '\n  '
         str_out += 'evolve_point: ' + str(self.evolve_point) + '\n  '
         str_out += 'evolve_branch: ' + str(self.evolve_branch) + '\n  '
-        str_out += 'evolver_cross: ' + str(self.evolver_cross) + '\n)'
+        str_out += 'evolver_cross: ' + str(self.evolve_cross) + '\n)'
         return str_out
 
     def _set_mratio(self, repro=0.15, point=0.15, branch=0.0, cross=0.70):
         """ Sets the mutation ratios, based on the given max population metric.
         """
-        self.evolve_repro = int(repro * self.tree_pop_max)
-        self.evolve_point = int(point * self.tree_pop_max)
-        self.evolve_branch = int(branch * self.tree_pop_max)
-        self.evolve_cross = int(cross * self.tree_pop_max)
-
-    def _set_kernel(self, kernel, max_inputs):
-        """ Sets up GP operators and methods, depending on the given kernel.
-        """
-        if kernel not in KERNEL_MODES:
-            raise AttributeError('Invalid kernel requested.')
-
-        # Gradient directions, by kernel
-        mode = {1: 'min',
-                2: 'max',
-                3: 'max'}
-
-        # Apply methods, by kernel
-        f_apply = {1: self._mode1_apply,
-                   2: self._mode2n3_apply,
-                   3: self._mode2n3_apply}
-
-        # Operators, by kernel
-        opers = {1: [['+', '2'],        
-                     ['-', '2'],
-                     ['*', '2'],
-                     ['/', '2']],
-                 2: [['+', '2']],
-                 3: [['+', '2'],        
-                     ['+ abs', '2']]}
-
-        # Kernel-dependent attributes
-        self._mode = kernel
-        self._f_apply = f_apply.get(kernel)
-        self.fitness_type = mode.get(kernel)
-        self.functions = array(opers.get(kernel))
-
-        # Kenrnel 1 gets a set of random floats from [0, 1)
-        if kernel == 1:
-            self.terminals = [random() for i in range(self.tree_pop_max)]
-
-        # Kernels 2 & 3 work with string inputs, so terminals must be
-        # non-numeric (sympy will simplify those) and non sympy function name
-        # (sympy will try to call those) placeholders. Ex: 'bzzz', 'gh', etc.
-        elif kernel == 2 or kernel == 3:
-            self.terminals = []
-            trailing_chars = 1
-            curr_char = 0
-            for _ in range(1, max_inputs + 1):
-                curr_char += 1
-                if curr_char >= 26:
-                    trailing_chars += 1
-                    curr_char = 1
-                ch = curr_char + 96
-                self.terminals.append(chr(ch) + chr(ch + 1) * trailing_chars)
-
-        #  Kernel independent attributes...
-        self.terminals += ['s']  # 's' label unused required by KarooGP
-        self.display = 's'                    # Silence Karoo GP menus/output
-        self.precision = 6                    # Tourney floating points
-
-        # Init first generation (if not already loaded)
+        # If not already initialized, assume a ratio, else assume integer
         try:
-            self.population_a
-            self.pop_tree_type = 'g'
+            self.evolve_repro  # throws except if not exists
+            self.evolve_repro = repro
+            self.evolve_point = point
+            self.evolve_branch = branch
+            self.evolve_cross = cross
         except AttributeError:
-            self.generation_id = 1
-            self.population_a = ['Generation 1']
-            self.fx_karoo_construct('r', 6)  # r = most diverse initial pop
-            for treeID in range(1, len(self.population_a)):
-                self.population_a[treeID][12][1] = 0.0
+            self.evolve_repro = int(repro * self.tree_pop_max)
+            self.evolve_point = int(point * self.tree_pop_max)
+            self.evolve_branch = int(branch * self.tree_pop_max)
+            self.evolve_cross = int(cross * self.tree_pop_max)
 
     def _tree_IDs(self):
         """ Returns a list of the populations tree IDs.
@@ -235,11 +206,11 @@ class Genetic(karoo_gp.Base_GP):
         writestr += ", 'tree_pop_max': " + str(self.tree_pop_max)
         writestr += ", 'generation_id': " + str(self.generation_id)
         writestr += ", 'pop_tree_type': '" + str(self.pop_tree_type) + "'"
-        writestr += ", 'population': \"" + str(self.population_a) + "\""
         writestr += ", 'evolve_repro': \"" + str(self.evolve_repro) + "\""
         writestr += ", 'evolve_point': \"" + str(self.evolve_point) + "\""
         writestr += ", 'evolve_branch': \"" + str(self.evolve_branch) + "\""
         writestr += ", 'evolve_cross': \"" + str(self.evolve_cross) + "\""
+        writestr += ", 'population': \"" + str(self.population_a) + "\""
         writestr += "}"
 
         if not filename:
@@ -267,35 +238,12 @@ class Genetic(karoo_gp.Base_GP):
         self.generation_id = data['generation_id']
         self.pop_tree_type = data['pop_tree_type']
         self.population_a = eval(data['population'])
-        self._set_mratio(data['evolve_repro'],
-                         data['evolve_point'],
-                         data['evolve_branch'],
-                         data['evolve_cross'])
+        self._set_mratio(int(data['evolve_repro']),
+                         int(data['evolve_point']),
+                         int(data['evolve_branch']),
+                         int(data['evolve_cross']))
 
-    def apply(self, **kwargs):
-        """ Applies the kernel to the given inputs (if any) and returns the
-            results as a list.
-        """
-        return self._f_apply(**kwargs)
-
-    def _mode1_apply(self, normalize=False):
-        """ Returns a list of numbers generated by each tree's expression.
-        """
-        results = []
-        trees = self._tree_IDs()
-        
-        for treeID in trees:
-            result = self._symp_expr(treeID)
-            results.append(result)
-
-        if normalize:
-            norm_max = max(results)
-            norm_min = min(results)
-            results = [(r - norm_min) / (norm_max - norm_min) for r in results]
-
-        return results
-
-    def _mode2n3_apply(self, inputs, is_seq=False):
+    def apply(self, inputs, is_seq=False):
         """ Applies each tree's expression to the given inputs.
             Accepts:
                 inputs (list)     : A list of lists, one for each input "row"
@@ -313,7 +261,7 @@ class Genetic(karoo_gp.Base_GP):
         trees = self._tree_IDs()
 
         # Iterate every tree that has made the cut
-        outputs = AttributesIter()
+        outputs = AttrIter()
         for treeID in trees:
             # Get current tree's expression. Ex: expr 'A + D'
             expression = self._expr_to_lst(f_getexpr(treeID))
@@ -353,11 +301,11 @@ class Genetic(karoo_gp.Base_GP):
                     output = ''.join(r)
 
                 # Results = output and contributing terminals, by treeID
-                outputs.push(treeID, ATTRIB_OUTPUT, output)
-                outputs.push(treeID, ATTRIB_INCONTEXT, list(in_context))
+                outputs.push(treeID, 'output', output)
+                outputs.push(treeID, 'from_inputs', list(in_context))
 
         # Remove empty results and return
-        outputs.rm_empties(ATTRIB_OUTPUT)
+        outputs.rm_empties('output')
         return outputs
 
     def update(self, fitness):
@@ -401,15 +349,15 @@ if __name__ == '__main__':
     is_seq = False   # Denote inputs should not be considered sequential
 
     # Init the genetically evolving expression trees (mode 2)
-    lengs = [len(inputs[i]) for i in range(len(inputs))]
+    lengs = [len(inputs[i]) for i in range(len(inputs))]  # max inputs length
     gp = Genetic(ID='treegp', 
-                 kernel=2,
+                 kernel=1,
                  max_pop=15, 
                  max_depth=4, 
                  max_inputs=max(lengs),
                  tourn_sz=min(lengs),
                  console_out=True, 
-                 persist=False)
+                 persist=True)
 
     # Get results, eval fitness, and backprogate fitness
     iterations = 30
@@ -422,7 +370,7 @@ if __name__ == '__main__':
         print('Results:')
         for trees in results:
             for treeID, attrs in trees.items():
-                output = attrs[ATTRIB_OUTPUT]
+                output = attrs['output']
                 pprint('Tree %d: %s' % (treeID, output))
 
                 # Evaluate fitness
